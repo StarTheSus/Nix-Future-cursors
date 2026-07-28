4667485fb0a7f64d137e9da888307354a4b35364
Star <252556693+StarTheSus@users.noreply.github.com>
2026-07-28 16:42:25 +0000

    fix(build): edit shebang to evaluate correctly

--- build.sh
+++ build.sh
@@ -1,62 +1,62 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 function create {
-	cd "$SRC"
-	mkdir -p x1 x1_25 x1_5 x2
-	cd "$SRC"/$1
-	find . -name "*.svg" -type f -exec sh -c 'inkscape -o "../x1/${0%.svg}.png" -w 32 -h 32 $0' {} \;
-	find . -name "*.svg" -type f -exec sh -c 'inkscape -o "../x1_25/${0%.svg}.png" -w 40 -w 40 $0' {} \;
-	find . -name "*.svg" -type f -exec sh -c 'inkscape -o "../x1_5/${0%.svg}.png" -w 48 -w 48 $0' {} \;
-	find . -name "*.svg" -type f -exec sh -c 'inkscape -o "../x2/${0%.svg}.png" -w 64 -w 64 $0' {} \;
+  cd "$SRC"
+  mkdir -p x1 x1_25 x1_5 x2
+  cd "$SRC"/$1
+  find . -name "*.svg" -type f -exec sh -c 'inkscape -o "../x1/${0%.svg}.png" -w 32 -h 32 $0' {} \;
+  find . -name "*.svg" -type f -exec sh -c 'inkscape -o "../x1_25/${0%.svg}.png" -w 40 -w 40 $0' {} \;
+  find . -name "*.svg" -type f -exec sh -c 'inkscape -o "../x1_5/${0%.svg}.png" -w 48 -w 48 $0' {} \;
+  find . -name "*.svg" -type f -exec sh -c 'inkscape -o "../x2/${0%.svg}.png" -w 64 -w 64 $0' {} \;
 
-	cd $SRC
+  cd $SRC
 
-	# generate cursors
-	BUILD="$SRC"/../dist
-	OUTPUT="$BUILD"/cursors
-	ALIASES="$SRC"/cursorList
+  # generate cursors
+  BUILD="$SRC"/../dist
+  OUTPUT="$BUILD"/cursors
+  ALIASES="$SRC"/cursorList
 
-	if [ ! -d "$BUILD" ]; then
-		mkdir "$BUILD"
-	fi
-	if [ ! -d "$OUTPUT" ]; then
-		mkdir "$OUTPUT"
-	fi
+  if [ ! -d "$BUILD" ]; then
+    mkdir "$BUILD"
+  fi
+  if [ ! -d "$OUTPUT" ]; then
+    mkdir "$OUTPUT"
+  fi
 
-	echo -ne "Generating cursor theme...\\r"
-	for CUR in config/*.cursor; do
-		BASENAME="$CUR"
-		BASENAME="${BASENAME##*/}"
-		BASENAME="${BASENAME%.*}"
-		
-		xcursorgen "$CUR" "$OUTPUT/$BASENAME"
-	done
-	echo -e "Generating cursor theme... DONE"
+  echo -ne "Generating cursor theme...\\r"
+  for CUR in config/*.cursor; do
+    BASENAME="$CUR"
+    BASENAME="${BASENAME##*/}"
+    BASENAME="${BASENAME%.*}"
 
-	cd "$OUTPUT"	
+    xcursorgen "$CUR" "$OUTPUT/$BASENAME"
+  done
+  echo -e "Generating cursor theme... DONE"
 
-	#generate aliases
-	echo -ne "Generating shortcuts...\\r"
-	while read ALIAS; do
-		FROM="${ALIAS#* }"
-		TO="${ALIAS% *}"
+  cd "$OUTPUT"
 
-		if [ -e $TO ]; then
-			continue
-		fi
-		ln -sr "$FROM" "$TO"
-	done < "$ALIASES"
-	echo -e "Generating shortcuts... DONE"
+  #generate aliases
+  echo -ne "Generating shortcuts...\\r"
+  while read ALIAS; do
+    FROM="${ALIAS#* }"
+    TO="${ALIAS% *}"
 
-	cd "$PWD"
+    if [ -e $TO ]; then
+      continue
+    fi
+    ln -sr "$FROM" "$TO"
+  done <"$ALIASES"
+  echo -e "Generating shortcuts... DONE"
 
-	echo -ne "Generating Theme Index...\\r"
-	INDEX="$OUTPUT/../index.theme"
-	if [ ! -e "$OUTPUT/../$INDEX" ]; then
-		touch "$INDEX"
-		echo -e "[Icon Theme]\nName=$THEME\n" > "$INDEX"
-	fi
-	echo -e "Generating Theme Index... DONE"
+  cd "$PWD"
+
+  echo -ne "Generating Theme Index...\\r"
+  INDEX="$OUTPUT/../index.theme"
+  if [ ! -e "$OUTPUT/../$INDEX" ]; then
+    touch "$INDEX"
+    echo -e "[Icon Theme]\nName=$THEME\n" >"$INDEX"
+  fi
+  echo -e "Generating Theme Index... DONE"
 }
 
 # generate pixmaps from svg source
@@ -64,4 +64,3 @@ SRC=$PWD/src
 THEME="Future Cursors"
 
 create svg
-
